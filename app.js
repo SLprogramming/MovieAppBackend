51632e997da28226c5757ad132a9bb8b68a42fd1
Taing Linn Maung <slprogramming.dev@gmail.com>
2025-08-31 00:16:30 +0630

    cors fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json({limit:"50mb"}))
 app.use(cookieParser())
 
 const allowedOrigins = [
-  "https://movie-app-website-mu.vercel.app/login",
+  "https://movie-app-website-mu.vercel.app",
   "http://192.168.110.125:5173",
   "http://192.168.110.125:5174"
 ];
