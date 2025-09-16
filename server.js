d02657ff4460b91cf858c2d35d66013bd66e7799
Taing Linn Maung <slprogramming.dev@gmail.com>
2025-09-16 23:26:54 +0630

    change server

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ initSocket(server);
 
 //create server
 
-// app.listen(process.env.PORT,() => {
-//     console.log(`server is running on http://localhost:${process.env.PORT}`)
-//     connectDB()
-// })
-server.listen(process.env.PORT,process.env.LOCAL_IP_ADDRESS,() => {
-    console.log(`server is running on http://${process.env.LOCAL_IP_ADDRESS}:${process.env.PORT}`)
+app.listen(process.env.PORT,() => {
+    console.log(`server is running on http://localhost:${process.env.PORT}`)
     connectDB()
 })
+// server.listen(process.env.PORT,process.env.LOCAL_IP_ADDRESS,() => {
+//     console.log(`server is running on http://${process.env.LOCAL_IP_ADDRESS}:${process.env.PORT}`)
+//     connectDB()
+// })
